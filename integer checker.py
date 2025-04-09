9aa619cabff84139096446c670a70018d07a189f
Jethro-Waite <jethro.waite@papamoacollege.school.nz>
2025-04-10 10:07:11 +1200

    Create integer checker.py

---
+++ integer checker.py
@@ -0,0 +1,15 @@
+
+error = "please enter an integer larger / equal to 13"
+
+while True:
+    try:
+        game_goal = int(input("What is the game goal? "))
+
+        if game_goal < 13:
+            print(error)
+        else:
+            print(f"Game goal: {game_goal}")
+            break
+
+    except ValueError:
+        print(error)
